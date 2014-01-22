1e244af3ea18dd1149582eb13258651c0abe8c42
mgoenka <mgoenka@yahoo-inc.com>
2014-01-21 19:18:56 -0800

    App for fetching Rotten Tomatoes reviews

--- src/com/mgoenka/rottentomatoes/RottenTomatoesActivity.java
+++ src/com/mgoenka/rottentomatoes/RottenTomatoesActivity.java
@@ -1,22 +1,54 @@
 package com.mgoenka.rottentomatoes;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class RottenTomatoesActivity extends Activity {
 
+	private ListView lvMovies;
+	private BoxOfficeMoviesAdapter adapterMovies;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_rotten_tomatoes);
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_rotten_tomatoes);
+	    lvMovies = (ListView) findViewById(R.id.lvMovies);
+	    ArrayList<BoxOfficeMovie> aMovies = new ArrayList<BoxOfficeMovie>();
+	    adapterMovies = new BoxOfficeMoviesAdapter(this, aMovies);
+	    lvMovies.setAdapter(adapterMovies);
+	    
+        fetchBoxOfficeMovies();
 	}
+	
+    // Executes an API call to the box office endpoint, parses the results
+    // Converts them into an array of movie objects and adds them to the adapter
+    private void fetchBoxOfficeMovies() {
+    	RottenTomatoesClient client = new RottenTomatoesClient();
+        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.rotten_tomatoes, menu);
-		return true;
-	}
-
+        @Override
+        public void onSuccess(int code, JSONObject body) {
+            JSONArray items = null;
+            try {
+                // Get the movies json array
+                items = body.getJSONArray("movies");
+                // Parse json array into array of model objects
+                ArrayList<BoxOfficeMovie> movies = BoxOfficeMovie.fromJson(items);
+                // Load model objects into the adapter
+                    adapterMovies.addAll(movies);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
---
+++ src/com/mgoenka/rottentomatoes/RottenTomatoesClient.java
@@ -0,0 +1,26 @@
+package com.mgoenka.rottentomatoes;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class RottenTomatoesClient {
+    private final String API_KEY = "8htwqtwfmrg78p3ynx7pycwn";
+    private final String API_BASE_URL = "http://api.rottentomatoes.com/api/public/v1.0/";
+    private AsyncHttpClient client;
+
+    public RottenTomatoesClient() {
+        this.client = new AsyncHttpClient();
+    }
+    
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+    
+    // http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=<key>
+    public void getBoxOfficeMovies(JsonHttpResponseHandler handler) {
+        String url = getApiUrl("lists/movies/box_office.json");
+        RequestParams params = new RequestParams("apikey", API_KEY);
+        client.get(url, params, handler);
+    }
+}
